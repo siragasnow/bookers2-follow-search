2e551f3a5d9a45b610aaf221b494b9c8d6c5b414
siragasnow <siraga613@gmail.com>
2020-07-06 15:31:41 +0900

    [fix]不具合修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_022111) do
+ActiveRecord::Schema.define(version: 2020_07_06_061205) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_022111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
