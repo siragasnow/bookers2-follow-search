1da3b21296f8def8d705a77f34ec74f764f09dbc
siragasnow <siraga613@gmail.com>
2020-07-07 21:41:57 +0900

    [finish]いいね・コメント非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,26 @@
 class BookCommentsController < ApplicationController
+	before_action :authenticate_user!
 
-	def create
-		book = Book.find(params[:book_id])
-		comment = current_user.book_comments.new(book_comment_params)
-		comment.book_id = book.id
-		comment.save
-		redirect_to book_path(book)
-	end
+  	def create
+    	@book = Book.find(params[:book_id])
+    	@book_new = Book.new
+    	@book_comment = @book.book_comments.new(book_comment_params)
+    	@book_comment.user_id = current_user.id
+    	if @book_comment.save
+      		flash[:success] = "Comment was successfully created."
+    	else
+      		@book_comments = BookComment.where(id: @book)
+    	end
+  	end
 
-	def destroy
-		comment = current_user.book_comments.find_by(params[:book_comment_id])
-		comment.destroy
-		redirect_to book_path(params[:book_id])
-	end
+  	def destroy
+    	@book_comment = BookComment.find(params[:book_id])
+    	@book = @book_comment.book
+    	if @book_comment.user != current_user
+      		redirect_to request.referer
+    	end
+    	@book_comment.destroy
+  	end
 
 	private
 	def book_comment_params
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,23 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def redirect
+    case params[:redirect_id].to_i
+    when 0
+      redirect_to books_path
+    when 1
+      redirect_to book_path(@book)
+    end
   end
 end
